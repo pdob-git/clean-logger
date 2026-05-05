525e4fd6a7c703d92d1a36ca3e32ae3b7d1221ef
Pablo <75912982+pdob-git@users.noreply.github.com>
2026-05-05 11:54:31 +0200

    feat: Add duration time
    Added loggig of duration time.

--- src/clean_logger/presentation/cli.py
+++ src/clean_logger/presentation/cli.py
@@ -1,3 +1,4 @@
+import time
 import uuid
 
 from ..application.use_cases.create_order import CreateOrderUseCase
@@ -6,18 +7,40 @@ from ..infrastructure.logging.config import configure_logging
 from ..infrastructure.logging.context import correlation_id
 
 
-def main() -> None:
-    configure_logging()
-
-    correlation_id.set(str(uuid.uuid4()))
-
-    logger = PythonLoggerAdapter(__name__)
+def action(logger: PythonLoggerAdapter) -> None:
     logger.info("Program started")
 
     use_case = CreateOrderUseCase(logger_provider=PythonLoggerAdapter)
 
     use_case.execute("ORD-123")
     logger.info("Program completed")
+
+
+# noinspection PyStringConversionWithoutDunderMethod
+def configure_logger() -> PythonLoggerAdapter:
+    configure_logging()
+
+    uuid_: uuid.UUID = uuid.uuid4()
+    correlation_id.set(str(uuid_))
+
+    logger_adapter: PythonLoggerAdapter = PythonLoggerAdapter(__name__)
+    return logger_adapter
+
+
+def main() -> None:
+    # Action with start and end record
+
+    # Record start
+    start = time.perf_counter()
+    logger = configure_logger()
+
+    # your code here
+    action(logger)
+
+    # record end
+    end = time.perf_counter()
+
+    logger.info(f"Execution time: {end - start:.6f} seconds")
 
 
 if __name__ == "__main__":
